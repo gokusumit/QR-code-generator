57051c5c750868e0bb52835595dc2e5247eaeb6f
Sumit Verma <138857545+gokusumit@users.noreply.github.com>
2024-04-27 22:50:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let imgbox=document.querySelector("#imgbox");
+let qrimg=document.querySelector("#qrimg");
+let qrtext=document.querySelector("#qrtext");
+
+
+
+
+
+function generateQR(){
+
+    if(qrtext.value.length>0){
+        qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+        imgbox.classList.add("show-img");
+    
+
+    }
+    else{
+        qrtext.classList.add("error");
+        setTimeout(()=>{
+            qrtext.classList.remove("error");
+        },1000);
+    }
+
+  
+
+    
+};
